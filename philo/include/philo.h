497c381a6c5c3eda875f08627e1394497d8bbcf2
yush <yus-sato@kalyte.ro>
2024-09-11 19:04:00 +0900

    Update

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: yussato <yussato@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 17:23:04 by yussato           #+#    #+#             */
-/*   Updated: 2024/09/11 18:57:18 by yussato          ###   ########.fr       */
+/*   Updated: 2024/09/11 19:03:21 by yussato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,10 @@ typedef struct s_philo
 }	t_philo;
 
 int			ft_atoi(const char *str);
-long		ft_getms(void);
 void		ft_putstr_fd(const char *str, int fd);
 size_t		ft_strlen(const char *str);
+
+long		getms(void);
 
 t_config	*config_create(int ac, char *av[]);
 
