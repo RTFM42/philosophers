beb40e9f88837a9dc57d63f436c3f6a13bd7a2a9
yush <yus-sato@kalyte.ro>
2024-09-18 00:38:01 +0900

    Update: Usage

--- philo/util/usage.c
+++ philo/util/usage.c
@@ -6,7 +6,7 @@
 /*   By: yussato <yussato@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/22 21:30:08 by yushsato          #+#    #+#             */
-/*   Updated: 2024/08/29 23:45:12 by yussato          ###   ########.fr       */
+/*   Updated: 2024/09/18 00:37:38 by yussato          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	putusage(const char *fname)
 {
 	ft_putstr_fd("Usage: ", 2);
 	ft_putstr_fd(fname, 2);
-	ft_putstr_fd(" number_of_philosophers"
+	ft_putstr_fd(" number_of_philosophers "
 		"time_to_die time_to_eat time_to_sleep "
 		"[number_of_times_each_philosopher_must_eat]\n", 2);
 }
